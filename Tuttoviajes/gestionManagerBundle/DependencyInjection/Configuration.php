f6a293221d78b9b859116ef1a6fca03e7694c92d
David Perez <davidpv@gmail.com>
2014-12-16 15:37:00 +0100

    prog

--- Tuttoviajes/gestionManagerBundle/DependencyInjection/Configuration.php
+++ Tuttoviajes/gestionManagerBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('gestion_manager_api');
+        $rootNode = $treeBuilder->root('tuttoviajes_api');
 
         $rootNode
             ->children()
